020c4e0a558df36d1627de8d41d04de92212d7e3
HwangHotae <hotae0321@naver.com>
2026-03-15 22:06:24 +0900

    error

---
+++ app/(with-searchbar)/search/error.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  useEffect(() => {
+    console.error(error.message);
+  }, [error]);
+
+  return (
+    <div>
+      <h3>검색 과정에서 오류가 발생했습니다.</h3>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh(); // 현재 페이지에 필요한 서버컴포넌트들을 불러옴.
+            reset(); // reset 굳이 왜씀? => 에러상태를 초기화. 컴포넌트들을 다시 렌더링
+          });
+        }}
+      >
+        다시 시도
+      </button>
+    </div>
+  );
+}
